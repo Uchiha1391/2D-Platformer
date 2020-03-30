87117235a857d831b5407b8871010bfe6cfca452
Youssef-Hammad <youssef88999@gmail.com>
2020-03-30 22:52:54 +0200

    Added Enemy Health Bar. Fixed hit particle glitch after respawning

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,17 +7,51 @@ public class Enemy : MonoBehaviour
     [System.Serializable]
     public class EnemyStats
     {
-        public int Health = 100;
+        public int maxHealth = 100;
+        private int _curHealth;
+        public int curHealth
+        {
+            get { return _curHealth; }
+            set { _curHealth = Mathf.Clamp(value,0,maxHealth); }
+        }
+
+        public void Init()
+        {
+            curHealth = maxHealth;
+        }
+
     }
 
     public EnemyStats enemyStats = new EnemyStats();
 
+    [Header("Optional:")]
+    [SerializeField]
+    private StatusIndicator statusIndicator;
+
+    void Start()
+    {
+        enemyStats.Init();
+
+        if (statusIndicator != null)
+        {
+            statusIndicator.SetHealth(enemyStats.curHealth, enemyStats.maxHealth);
+        }
+    }
+
     public void DamageEnemy(int damage)
     {
-        enemyStats.Health -= damage;
-        if(enemyStats.Health<=0)
+        enemyStats.curHealth -= damage;
+        if(enemyStats.curHealth<=0)
         {
             GameMaster.KillEnemy(this);
         }
+        if (statusIndicator != null)
+        {
+            statusIndicator.SetHealth(enemyStats.curHealth, enemyStats.maxHealth);
+        }
+    }
+    void Update()
+    {
+
     }
 }
